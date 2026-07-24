ae9bf38759c749e2ed41d88e85089f2f3e6571a8
hopebeanne025-tech <hopebeanne025@gmail.com>
2026-07-25 00:51:14 +0800

    Enhance video swiper with navigation and autoplay
    Added navigation controls and autoplay settings to the video swiper.

--- script.js
+++ script.js
@@ -67,3 +67,42 @@ document.addEventListener("DOMContentLoaded", () => {
     counters.forEach(counter => observer.observe(counter));
 
 });
+const swiper = new Swiper(".videoSwiper",{
+
+    loop:true,
+
+    speed:700,
+
+    autoplay:{
+        delay:5000,
+        disableOnInteraction:false
+    },
+
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    },
+
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+
+});
+const videoSwiper = new Swiper(".videoSwiper", {
+
+    loop: true,
+
+    allowTouchMove: false, // <-- idagdag ito
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+
+});
